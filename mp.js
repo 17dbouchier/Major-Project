945a4616a87ff9537018596732259d77c4df778e
Dillon Bouchier <dillonbouchier@Macbook-Pro.local>
2021-09-15 01:47:06 +1200

    Added progress across 5 members, adding portfolio bug / improved navbar

--- mp.js
+++ mp.js
@@ -1,11 +1,11 @@
 window.onscroll = function() {scrollFunction()};
 
-document.getElementById("navbar").style.top = "-70px";
+document.getElementById("navbar").style.top = "-100px";
 
 function scrollFunction() {
   if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-70px";
+    document.getElementById("navbar").style.top = "-100px";
   }
 }
